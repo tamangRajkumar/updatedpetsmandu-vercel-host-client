13e3a9d3b26294fe55d914896d2814dceba70a7f
DELL <rajkumartamang182@gmail.com>
2022-08-03 13:07:15 +0545

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,14 +8,7 @@ const initialState = {};
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(thunk),
-
-    process.env.NODE_ENV === "development"
-      ? window.__REDUX_DEVTOOLS_EXTENSION__ &&
-          window.__REDUX_DEVTOOLS_EXTENSION__()
-      : null
-  )
+  compose(applyMiddleware(thunk))
 );
 
 export default store;
