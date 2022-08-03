9bdbdc4e5138b443e91a11257cafb13f4a56ee97
DELL <rajkumartamang182@gmail.com>
2022-08-03 13:21:30 +0545

    updated

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,13 +2,20 @@ import { createStore, applyMiddleware, compose } from "redux";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 // import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+
 
 const initialState = {};
 
 const store = createStore(
   rootReducer,
   initialState,
-  compose(applyMiddleware(thunk))
+  composeWithDevTools(
+    applyMiddleware(thunk),
+    
+    // window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  ),
+  
 );
 
 export default store;
